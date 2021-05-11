1b2091759175fe1f1586a642d3962d5e23f851b6
ikhbaaalll <ikhbal.madrid17@gmail.com>
2021-05-11 20:12:41 +0700

    update Route

--- src/Route/index.js
+++ src/Route/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
-import {StyleSheet, Text, View} from 'react-native';
-import {createStackNavigator} from '@react-navigation/stack';
-import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
-import { Kelas, Login, Profil, QrScan, SplashScreen } from '../pages';
+import { StyleSheet, Text, View } from 'react-native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { Kelas, Login, Profil, QrScan, SplashScreen, Auth } from '../pages';
 import { BottomNavigator } from '../components'
 
 const Stack = createStackNavigator();
@@ -11,9 +11,9 @@ const Tab = createBottomTabNavigator();
 const MainApp = () => {
   return (
     <Tab.Navigator tabBar={props => <BottomNavigator {...props} />} initialRouteName="QrScan">
-      <Tab.Screen name="Profil" component={Profil} style={styles.menu}/>
-      <Tab.Screen name="QrScan" component={QrScan} style={styles.menu}/>
-      <Tab.Screen name="Kelas" component={Kelas} style={styles.menu}/>
+      <Tab.Screen name="Profil" component={Profil} style={styles.menu} />
+      <Tab.Screen name="QrScan" component={QrScan} style={styles.menu} />
+      <Tab.Screen name="Kelas" component={Kelas} style={styles.menu} />
     </Tab.Navigator>
   );
 };
@@ -24,17 +24,22 @@ const Route = () => {
       <Stack.Screen
         name="SplashScreen"
         component={SplashScreen}
-        options={{headerShown: false}}
+        options={{ headerShown: false }}
+      />
+      <Stack.Screen
+        name="Auth"
+        component={Auth}
+        options={{ headerShown: false }}
       />
       <Stack.Screen
         name="Login"
         component={Login}
-        options={{headerShown: false}}
+        options={{ headerShown: false }}
       />
       <Stack.Screen
         name="MainApp"
         component={MainApp}
-        options={{headerShown: false}}
+        options={{ headerShown: false }}
       />
     </Stack.Navigator>
   );
