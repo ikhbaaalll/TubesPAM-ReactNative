b6e3b6e63c139df811a7097a61236f0b8f7046b9
ikhbaaalll <ikhbal.madrid17@gmail.com>
2021-05-11 20:12:53 +0700

    update pages

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,5 +3,6 @@ import Login from './Login';
 import Profil from './Profil';
 import QrScan from './QrScan';
 import SplashScreen from './SplashScreen';
+import Auth from './Auth'
 
-export { Kelas, Login, Profil, QrScan, SplashScreen }
+export { Kelas, Login, Profil, QrScan, SplashScreen, Auth }
