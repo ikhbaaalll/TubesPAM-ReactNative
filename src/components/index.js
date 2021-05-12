4aca4daebdb8ee1a2a5265c5ea8cb988ab9d61a3
ryznocaHub <fikrihalim27@gmail.com>
2021-05-12 22:23:55 +0700

    add import component

--- src/components/index.js
+++ src/components/index.js
@@ -4,5 +4,7 @@ import ButtonIcon from './ButtonIcon'
 import MyDatePicker from './MyDatePicker'
 import PickDateTime from './PickDateTime'
 import ButtonCustom from './ButtonCustom'
+import ButtonPertemuan from './ButtonPertemuan'
+import ArrowBack from './ArrowBack'
 
-export { BottomNavigator, ButtonIcon, TabIcon, MyDatePicker, PickDateTime, ButtonCustom }
+export { BottomNavigator, ButtonIcon, TabIcon, MyDatePicker, PickDateTime, ButtonCustom, ButtonPertemuan, ArrowBack }
--- src/pages/index.js
+++ src/pages/index.js
@@ -5,5 +5,7 @@ import QrScan from './QrScan';
 import SplashScreen from './SplashScreen';
 import Auth from './Auth'
 import KelasTambah from './KelasTambah'
+import KelasList from './KelasList'
+import KelasPertemuan from './KelasPertemuan'
 
-export { Kelas, Login, Profil, QrScan, SplashScreen, Auth, KelasTambah }
+export { Kelas, Login, Profil, QrScan, SplashScreen, Auth, KelasTambah, KelasList, KelasPertemuan }
