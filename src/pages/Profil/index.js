760905f2ec163b916d4d51d57ae3a08476d1c132
ikhbaaalll <ikhbal.madrid17@gmail.com>
2021-05-14 22:17:52 +0700

    update page Profil

--- src/pages/Profil/index.js
+++ src/pages/Profil/index.js
@@ -1,12 +1,25 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { StyleSheet, Text, View, Button } from 'react-native'
 import AsyncStorage from '@react-native-community/async-storage';
 
 const Profil = ({ navigation }) => {
+
     const logout = async () => {
         await AsyncStorage.clear()
         navigation.navigate("Login")
     }
+
+    useEffect(() => {
+        const _getUser = async () => {
+            const user = await AsyncStorage.getItem('id')
+            if (!user) {
+                navigation.replace('Login')
+            }
+            console.log(user)
+        }
+        _getUser()
+
+    }, [])
 
     return (
         <View>
