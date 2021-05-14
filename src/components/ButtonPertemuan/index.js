c6f687d333ddf22b5b9b76a1f60a2ff1f755bb9d
ryznocaHub <fikrihalim27@gmail.com>
2021-05-14 16:17:00 +0700

    Add Modal QrCode pada Button Pertemuan

--- src/components/ButtonPertemuan/index.js
+++ src/components/ButtonPertemuan/index.js
@@ -1,17 +1,27 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {
   StyleSheet,
   Text,
   View,
   TouchableOpacity,
   TouchableHighlight,
+  Button,
+  ScrollView,
 } from 'react-native';
+import Modal from 'react-native-modal';
 import {ColorPrimary, ColorSecondary} from '../../utils/constanta';
-import {Qrcode, QrcodeActive} from '../../assets';
+import {Qrcode, QrcodeActive, Mola} from '../../assets';
+import {ButtonCustom, QrCode} from '../../components';
 
 const ButtonPertemuan = ({topik}) => {
   var [isPress, setIsPress] = React.useState(false);
+  var [isQrPress, setIsQrPress] = React.useState(false);
+  var [isModalPress, setIsModalPress] = React.useState(false);
+  const [isModalVisible, setModalVisible] = useState(false);
 
+  const toggleModal = () => {
+    setModalVisible(!isModalVisible);
+  };
   var touchProps = {
     activeOpacity: 1,
     underlayColor: 'white',
@@ -19,7 +29,6 @@ const ButtonPertemuan = ({topik}) => {
     onShowUnderlay: () => setIsPress(true),
     onPress: () => console.log('Button Icon Pressed'),
   };
-  var [isQrPress, setIsQrPress] = React.useState(false);
 
   var qrProps = {
     activeOpacity: 1,
@@ -28,6 +37,17 @@ const ButtonPertemuan = ({topik}) => {
     onShowUnderlay: () => setIsQrPress(true),
     onPress: () => console.log('Button qr Pressed'),
   };
+
+  var modalProps = {
+    activeOpacity: 1,
+    underlayColor: 'white',
+    onHideUnderlay: () => setIsModalPress(false),
+    onShowUnderlay: () => setIsModalPress(true),
+    onPress: () => console.log('Button modal Pressed'),
+    // setModalVisible(!isModalVisible);
+  };
+
+
   return (
     <TouchableHighlight {...touchProps}>
       <View style={isPress ? styles.containerBaru : styles.container}>
@@ -35,11 +55,29 @@ const ButtonPertemuan = ({topik}) => {
           <Text style={styles.title}>Pertemuan 1</Text>
           <Text style={styles.topik}>{topik}</Text>
         </View>
-        <TouchableHighlight {...qrProps}>
+        <TouchableHighlight {...qrProps} onPress={toggleModal}>
           <View style={isQrPress ? styles.boxIconBaru : styles.boxIcon}>
-          {isQrPress ? <QrcodeActive /> : <Qrcode />}
+            {isQrPress ? <QrcodeActive /> : <Qrcode />}
           </View>
         </TouchableHighlight>
+
+        <Modal isVisible={isModalVisible}>
+          <ScrollView style={styles.modal}>
+          {/* Ganti gambar di bagian source */}
+            <QrCode source={Mola}/>
+            <Text style={styles.nip}>Pelajaran</Text>
+            <Text style={styles.nama}>
+              Pendidikan Jasmani, Olahraga, dan Kesehatan
+            </Text>
+            <Text style={styles.nip}>Topik</Text>
+            <Text style={styles.nama}>
+              Kebugaran Jasmani dengan permainan bola besar
+            </Text>
+            <TouchableHighlight {...modalProps} onPress={toggleModal} style={{marginBottom:50}}>
+              <ButtonCustom title="Tutup" isPress={isModalPress} />
+            </TouchableHighlight>
+          </ScrollView>
+        </Modal>
       </View>
     </TouchableHighlight>
   );
@@ -72,9 +110,7 @@ const styles = StyleSheet.create({
     marginHorizontal: 10,
   },
   containerDalam: {
-    // backgroundColor: '#31ec92',
     flex: 7,
-    // paddingRight: 10,
   },
   boxIcon: {
     justifyContent: 'center',
@@ -96,7 +132,6 @@ const styles = StyleSheet.create({
     marginLeft: 20,
     borderColor: ColorPrimary,
     borderWidth: 2,
-    // elevation: 5,
   },
   title: {
     fontWeight: 'bold',
@@ -106,5 +141,42 @@ const styles = StyleSheet.create({
   topik: {
     color: '#000',
     fontSize: 15,
+  },
+  modal: {
+    flex: 1,
+    backgroundColor: '#fff',
+    padding: 30,
+    // justifyContent: 'center',
+    // alignItems: 'center',
+    // paddingBottom: 100,
+  },
+  modalBox: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginBottom: 20,
+  },
+  imageBox: {
+    height: 250,
+    width: 250,
+    marginBottom: 20,
+  },
+  buttonBox: {
+    backgroundColor: '#31ec92',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  textButton: isEnabled => ({
+    fontSize: 15,
+    color: isEnabled ? ColorPrimary : '#f58ca1',
+  }),
+  nama: {
+    color: '#4d4d4d',
+    fontWeight: 'bold',
+    fontSize: 18,
+  },
+  nip: {
+    color: ColorPrimary,
+    fontSize: 15,
+    marginTop: 15,
   },
 });
