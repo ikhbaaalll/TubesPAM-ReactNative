ee7be9d419209811630a79ee4c0e5e6fdd3c4e35
ikhbaaalll <ikhbal.madrid17@gmail.com>
2021-05-15 00:26:11 +0700

    update QrCode component fetch api set status qr code

--- src/components/QrCode/index.js
+++ src/components/QrCode/index.js
@@ -1,10 +1,29 @@
-import React, {useState} from 'react';
-import {StyleSheet, Text, View, ImageBackground, Switch} from 'react-native';
-import {ColorPrimary, ColorSecondary} from '../../utils/constanta';
+import React, { useState, useEffect } from 'react';
+import { StyleSheet, Text, View, ImageBackground, Switch } from 'react-native';
+import { ColorPrimary, ColorSecondary } from '../../utils/constanta';
 
-const QrCode = ({source}) => {
-  const [isEnabled, setIsEnabled] = useState(false);
-  const toggleSwitch = () => setIsEnabled(previousState => !previousState);
+const QrCode = ({ source, status, id }) => {
+  const [isEnabled, setIsEnabled] = useState(status == '1' ? true : false);
+  const [stat, setStat] = useState(status)
+  const toggleSwitch = () => {
+    setIsEnabled(previousState => !previousState)
+    setStat(isEnabled ? '1' : '0')
+    fetch('http://192.168.43.152:1010/api/kelas/status', {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        id: id,
+        status: stat == '1' ? '0' : '1'
+      }),
+    })
+      .then(response => response.json())
+      .then(responseJson => {
+        setStat(responseJson.status.status)
+      })
+  };
 
   return (
     <View style={styles.modalBox}>
@@ -13,7 +32,7 @@ const QrCode = ({source}) => {
         source={source}
         style={styles.imageBox}></ImageBackground>
       <Switch
-        trackColor={{false: '#f58ca1', true: ColorPrimary}}
+        trackColor={{ false: '#f58ca1', true: ColorPrimary }}
         thumbColor={isEnabled ? '#f5dd4b' : '#f4f3f4'}
         ios_backgroundColor="#3e3e3e"
         onValueChange={toggleSwitch}
