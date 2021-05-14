51a3b5026e8aaec70b3d575782a8780c1ea2c0c0
ryznocaHub <fikrihalim27@gmail.com>
2021-05-14 20:24:45 +0700

    View Home Guru

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,5 +8,6 @@ import KelasTambah from './KelasTambah'
 import KelasList from './KelasList'
 import KelasPertemuan from './KelasPertemuan'
 import KelasDetail from './KelasDetail'
+import HomeGuru from './HomeGuru'
 
-export { Kelas, Login, Profil, QrScan, SplashScreen, Auth, KelasTambah, KelasList, KelasPertemuan, KelasDetail }
+export { Kelas, Login, Profil, QrScan, SplashScreen, Auth, KelasTambah, KelasList, KelasPertemuan, KelasDetail, HomeGuru }
