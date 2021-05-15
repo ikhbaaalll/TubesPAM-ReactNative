a0f3aaadd895543d0b01fcc112bce7510c91e6e0
ryznocaHub <fikrihalim27@gmail.com>
2021-05-16 02:32:09 +0700

    add import pages

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,9 +5,14 @@ import QrScan from './QrScan';
 import SplashScreen from './SplashScreen';
 import Auth from './Auth'
 import KelasTambah from './KelasTambah'
+import KelasHapus from './KelasHapus'
 import KelasList from './KelasList'
 import KelasPertemuan from './KelasPertemuan'
 import KelasDetail from './KelasDetail'
 import HomeGuru from './HomeGuru'
+import UserTambah from './UserTambah'
+import UserKelas from './UserKelas'
+import UserList from './UserList'
 
-export { Kelas, Login, Profil, QrScan, SplashScreen, Auth, KelasTambah, KelasList, KelasPertemuan, KelasDetail, HomeGuru }
+export { Kelas, Login, Profil, QrScan, SplashScreen, Auth, KelasTambah, KelasList, KelasPertemuan, KelasDetail, HomeGuru, KelasHapus, UserTambah, UserKelas, UserList
+UserList }
