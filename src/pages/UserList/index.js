642178220286159e1241c048b21237ba8ec279d3
ikhbaaalll <ikhbal.madrid17@gmail.com>
2021-05-16 07:43:03 +0700

    update page UserList

--- src/pages/UserList/index.js
+++ src/pages/UserList/index.js
@@ -1,22 +1,47 @@
-import React from 'react';
-import {StyleSheet, Text, View, ScrollView} from 'react-native';
-import {ColorPrimary, ColorSecondary} from '../../utils/constanta';
-import {ArrowBack, ItemSiswa} from '../../components';
+import React, { useState, useEffect } from 'react';
+import { StyleSheet, Text, View, ScrollView } from 'react-native';
+import { ColorPrimary, ColorSecondary } from '../../utils/constanta';
+import { ArrowBack, Siswa } from '../../components';
 
-const UserList = () => {
+const UserList = ({ route, navigation }) => {
+  const { kelas, userKelas } = route.params
+  const [getKelas, setKelas] = useState(JSON.stringify(kelas).replace(/\"/g, ""))
+  const [getUserKelas, setUserKelas] = useState(JSON.stringify(userKelas).replace(/\"/g, ""))
+  const [listUser, setListUser] = useState([])
+
+  useEffect(() => {
+    fetch('http://192.168.43.152:1010/api/user/index', {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        kelas: getKelas
+      }),
+    })
+      .then(response => response.json())
+      .then(responseJson => {
+        setListUser(responseJson), console.log(responseJson)
+      })
+
+  }, [])
+
   return (
     <View style={styles.container}>
       <View style={styles.header}>
         <ArrowBack />
-        <Text style={styles.text_header}>Siswa Kelas 1</Text>
+        <Text style={styles.text_header}>Siswa Kelas {getKelas}</Text>
       </View>
       <View style={styles.footer}>
         <View style={styles.boxShadow}></View>
         <ScrollView>
           <View style={styles.footerBox}>
-            {/* <ItemSiswa key={data.id} nama={data.user.nama} status={data.status} */}
-            <ItemSiswa status="3" nama="fikri" />
-            <ItemSiswa status="3" nama="fikri" />
+            {
+              listUser.map(data => {
+                return <Siswa key={data.id} nama={data.nama} id={data.id} kelas={data.kelas} userKelas={getUserKelas} email={data.email} />
+              })
+            }
           </View>
         </ScrollView>
       </View>
@@ -68,7 +93,7 @@ const styles = StyleSheet.create({
     // backgroundColor: ColorPrimary
   },
   boxShadow: {
-     borderWidth: 2,
+    borderWidth: 2,
     borderRadius: 50,
     borderColor: ColorPrimary,
     shadowColor: '#005343',
