6df0af18f4fcde2979eba7d0e48a3cfe40a5f5d1
annisa118140082 <annisa.118140082@student.itera.ac.id>
2021-05-17 07:53:36 +0700

    update component index
    menghapus unused component

--- src/components/index.js
+++ src/components/index.js
@@ -1,8 +1,6 @@
 import BottomNavigator from './BottomNavigator';
 import TabIcon from './TabIcon';
 import ButtonIcon from './ButtonIcon';
-import MyDatePicker from './MyDatePicker';
-import PickDateTime from './PickDateTime';
 import ButtonCustom from './ButtonCustom';
 import ButtonPertemuan from './ButtonPertemuan';
 import ArrowBack from './ArrowBack';
@@ -17,8 +15,6 @@ export {
   BottomNavigator,
   ButtonIcon,
   TabIcon,
-  MyDatePicker,
-  PickDateTime,
   ButtonCustom,
   ButtonPertemuan,
   ArrowBack,
